7405c59760738751cda5097d56628162aa2d6a5e
Pooja Rani T S <81300487+taluripooja@users.noreply.github.com>
2024-09-04 12:10:13 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -15,6 +15,34 @@ class TestSoundex(unittest.TestCase):
     def test_four_character(self):
         self.assertEqual(generate_soundex("BFPV"), "B000")
         
-    
+    def test_string_with_special_characters(self):
+        self.assertEqual(generate_soundex("1@!Abc3"),"1120")
+        
+    def test_string_with_vowels(self):
+        self.assertEqual(generate_soundex("Robert"),"R163")
+
+    def test_string_with_numbers(self):
+        self.assertEqual(generate_soundex("1234"),"1000")     
+
+    def test_names_with_special_characters(self):
+        self.assertEqual(generate_soundex("O'Connor"), "O252")
+        self.assertEqual(generate_soundex("Anne-Marie"), "A052")
+
+    def test_names_with_numbers(self):
+        self.assertEqual(generate_soundex("123"), "0000")
+        self.assertEqual(generate_soundex("John123"), "J053")
+
+    def test_long_name(self):
+        self.assertEqual(generate_soundex("Christopher"), "C362")
+
+    def test_names_with_repeated_letters(self):
+        self.assertEqual(generate_soundex("Bobby"), "B010")
+
+    def test_names_with_h_and_w(self):
+        self.assertEqual(generate_soundex("Hugh"), "H020")
+
+    def test_failure_case(self):
+        self.assertNotEqual(generate_soundex("Honeyman"), "H554")
+
 if __name__ == '__main__':
     unittest.main()
