25fe88ea1bd02c89c9c1e5e7b1fa5f5953063258
Pooja Rani T S <81300487+taluripooja@users.noreply.github.com>
2024-09-04 12:00:20 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,21 +1,38 @@
+def get_soundex_code(c):
+    c = c.upper()
+    mapping = {
+        'B': '1', 'F': '1', 'P': '1', 'V': '1',
+        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+        'D': '3', 'T': '3',
+        'L': '4',
+        'M': '5', 'N': '5',
+        'R': '6'
+    }
+    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
+
+def truncate(soundex):
+    if len(soundex)>=4:
+        soundex=soundex[0:4]
+    else:
+        soundex=soundex.ljust(4,'0')
+    return soundex
+
+def set_soundex_char(code,prev_code,soundex):
+    if code!='0' and code !=prev_code:
+        soundex += code
+        prev_code = code
+    return soundex,prev_code
+
 def generate_soundex(name):
     if not name:
         return ""
 
-    soundex = [name[0].upper()]
-    prev_code = get_soundex_code(soundex[0])
+    # Start with the first letter (capitalized)
+    soundex = name[0].upper()
+    prev_code = get_soundex_code(soundex)
 
-    def add_soundex_code(char, prev_code):
+    for char in name[1:]:
         code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex.append(code)
-        return code
-
-    prev_code = add_soundex_code(name[1], prev_code) if len(name) > 1 else prev_code
-
-    for char in name[2:]:
-        prev_code = add_soundex_code(char, prev_code)
-        if len(soundex) == 4:
-            break
-
-    return ''.join(soundex).ljust(4, '0')
+        soundex,prev_code=set_soundex_char(code,prev_code,soundex)            
+    soundex=truncate(soundex)
+    return soundex
