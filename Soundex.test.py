12ba9329cc3beac1e2ee431c7972a3b04f5f1f70
Pooja Rani T S <81300487+taluripooja@users.noreply.github.com>
2024-09-04 12:04:44 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,6 +9,12 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("FGTM"), "F235")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("BFPV"), "B000")
+        
     
 if __name__ == '__main__':
     unittest.main()
