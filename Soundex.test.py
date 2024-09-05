5b55ca8f21999591cb3546baa0e25533c72a13bf
Pooja Rani T S <81300487+taluripooja@users.noreply.github.com>
2024-09-05 14:45:14 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -23,10 +23,24 @@ class TestSoundex(unittest.TestCase):
 
     def test_string_with_numbers(self):
         self.assertEqual(generate_soundex("1234"),"1000")     
-
-
+        
     def test_failure_case(self):
         self.assertNotEqual(generate_soundex("Honeyman"), "H554")
+
+    def test_repeated_characters(self):
+        self.assertEqual(generate_soundex("Tomm"), "T500")
+
+    def test_name_starting_with_vowel(self):
+        self.assertEqual(generate_soundex("Andrew"), "A536")
+
+    def test_same_consonant_group(self):
+        self.assertEqual(generate_soundex("Pfister"), "P236")
+
+    def test_multiple_vowels(self):
+        self.assertEqual(generate_soundex("Euler"), "E460")
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
